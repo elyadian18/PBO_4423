fb5b89b1781403677bad1cf6bdac228ce22869ab
elyadian18 <75715565+elyadian18@users.noreply.github.com>
2021-03-06 12:30:15 +0700

    Update TugasW1.java

--- src/TugasW1.java
+++ src/TugasW1.java
@@ -1,13 +1,13 @@
 public class TugasW1 {
     public static void main(String[] args) {
-        int Soal_a = -5 + 8 *6;
-        int Soal_b= (55+9)%9;
-        int Soal_c = 20 + -3*5 /8;
-        int soal_d =  5 + 15 / 3 * 2 - 8 % 3;
+        int a = -5 + 8 *6;
+        int b = (55+9)%9;
+        int c = 20 + -3*5 /8;
+        int d =  5 + 15 / 3 * 2 - 8 % 3;
 
-        System.out.println(Soal_a);
-        System.out.println(Soal_b);
-        System.out.println(Soal_c);
-        System.out.println(soal_d);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
