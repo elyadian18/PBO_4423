9ab7958e4ca5d2b015471358317330924ddfcfff
elyadian18 <elyadian18@gmail.com>
2021-03-06 11:56:32 +0700

    ubah repo tugas

---
+++ src/TugasW1.java
@@ -0,0 +1,13 @@
+public class TugasW1 {
+    public static void main(String[] args) {
+        int Soal_a = -5 + 8 *6;
+        int Soal_b= (55+9)%9;
+        int Soal_c = 20 + -3*5 /8;
+        int soal_d =  5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println(Soal_a);
+        System.out.println(Soal_b);
+        System.out.println(Soal_c);
+        System.out.println(soal_d);
+    }
+}
